222bf935415856059998ea02dd2a47d3eca99c61
ndarkangel2 <nuz7b@mail.umkc.edu>
2018-06-15 00:52:45 -0500

    newupdate

--- Source/Web_Lesson4_SourceCode/ICP/Todo/todo.js
+++ Source/Web_Lesson4_SourceCode/ICP/Todo/todo.js
@@ -1,23 +1,30 @@
-var angularTodo = angular.module('angularTodo', []);
+var angularTodo = angular.module('toDoApp', []);
 
 angularTodo.controller('angularTodoC', ['$scope', function ($scope) {
     // define list of items
-    $scope.items = [];
+    $scope.items = [{todoText:'List Start Here', done:false}];
 
     // Write code to push new item
 
     $scope.submitNewItem = function () {
-
+        $scope.items.push({todoText:$scope.input, done:false});
+        $scope.input = "";
     };
 
     // Write code to complete item
     $scope.completeItem = function (index) {
-
+        $scope.items[index].completed = !$scope.items[index].completed;
     };
 
     // Write code to delete item
 
-    $scope.deleteItem = function (index) {
+    $scope.deleteItem= function (index) {
+
+        $scope.items.splice(index,1);
+
+
+
 
     };
+
 }]);
