d4c5b789b2da45d55d5b48b75d1a1c72804265d3
ndarkangel2 <nuz7b@mail.umkc.edu>
2018-06-15 12:16:36 -0500

    new update

--- Source/Web_Lesson4_SourceCode/ICP/Calculator/calculator.js
+++ Source/Web_Lesson4_SourceCode/ICP/Calculator/calculator.js
@@ -1,31 +1,139 @@
 angular.module('CalculatorApp', [])
     .controller('CalculatorController', function ($scope) {
-        // Write code simple calculator operations
-        $scope.a = 0;
-        $scope.b = 0;
-        $scope.fistscope = function() {
-            return $scope.a - 0;
+
+        var elementsArray = [];
+
+        clear();
+
+        function clear() {
+            $scope.equation = "";
+            $scope.number = 0;
+            elementsArray = [];
+        }
+        $scope.buttonClicked = function (btn){
+            $scope.equation + btn;
+
         }
 
-        $scope.secoundscope = function() {
-            return $scope.b - 0;
+
+        function clearNumber() {
+            $scope.number = "";
+        };
+
+        function equationAdd(a, b) {
+            elementsArray.push(a);
+            elementsArray.push(b);
+            $scope.equation += a;
+            $scope.equation += b;
         }
-        $scope.result= 0;
+
+        function multiply(a, b) {
+            var c = parseInt(elementsArray[a]) * parseInt(elementsArray[b]);
+            elementsArray[a] = c.toString();
+            elementsArray.splice(a + 1, 2);
+        }
+
+        function divide(a, b) {
+            var c = parseInt(elementsArray[a]) / parseInt(elementsArray[b]);
+            elementsArray[a] = c.toString();
+            elementsArray.splice(a + 1, 2);
+        }
+
+        function add(a, b) {
+            var c = parseInt(elementsArray[a]) + parseInt(elementsArray[b]);
+            elementsArray[a] = c.toString();
+            elementsArray.splice(a + 1, 2);
+        }
+
+        function substract(a, b) {
+            var c = parseInt(elementsArray[a]) - parseInt(elementsArray[b]);
+            elementsArray[a] = c.toString();
+            elementsArray.splice(a + 1, 2);
+        }
 
 
+        function calculate() {
+            for (var i = 0; i < elementsArray.length; i++) {
+                if (elementsArray[i] == "*") {
+                    multiply(i - 1, i + 1);
+                    i = i - 2;
+                } else if (elementsArray[i] == "/") {
+                    divide(i - 1, i + 1);
+                    i = i - 2;
+                }
 
-        $scope.result = function() {
-            if ($scope.buttons== '*') {
-                return $scope.a * $scope.b;
-            }
-            if ($scope.buttons == '/') {
-                return $scope.a / $scope.b;
-            }
-            if ($scope.buttons == '+') {
-                return $scope.a + $scope.b;
-            }
-            if ($scope.buttons == '-') {
-                return $scope.a - $scope.b;
+            };
+            for (var i = 0; i < elementsArray.length; i++) {
+                if (elementsArray[i] == "+") {
+                    add(i - 1, i + 1);
+                    i = i - 2;
+                } else if (elementsArray[i] == "-") {
+                    substract(i - 1, i + 1);
+                    i = i - 2;
+                }
             }
         };
+
+        for (var i = 0; i < button.length; i++) {
+            button[i].addEventListener('click', function() {
+                switch (this.innerHTML) {
+                    case "+":
+                    {
+                        equationAdd($scope.number, "+");
+                        clearNumber();
+                        break;
+                    }
+                    case "-":
+                    {
+                        equationAdd($scope.number, "-");
+                        clearNumber();
+                        break;
+                    }
+
+                    case "*":
+                    {
+                        equationAdd($scope.number, "*");
+                        clearNumber();
+                        break;
+                    }
+                    case "/":
+                    {
+                        equationAdd($scope.number, "/");
+                        clearNumber();
+                        break;
+                    }
+
+                    case "%":
+                    {
+                        equationAdd($scope.number, "%");
+                        clearNumber();
+                        break;
+                    }
+                    case "Clear":
+                    {
+                        clear();
+                        break;
+
+                    }
+
+                    case "=":
+                    {
+                        equationAdd($scope.number, "=");
+                        elementsArray.pop();
+                        calculate();
+                        $scope.number = elementsArray[0];
+                        $scope.equation = "";
+                        elementsArray.pop();
+                        break;
+                    }
+
+                    default:
+                    {
+                        $scope.number += this.innerHTML;
+                        break;
+                    };
+
+                };
+            });
+        }
     });
